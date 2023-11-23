034870f36753977994358a660571a045e7a72dce
Am <arphasirisubb@gmail.com>
2023-11-24 01:23:23 +0700

    submit boolean assignment

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,12 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
-
+// Exercise #1: Event Conditions
 // Start coding here
+let isOver18 = false;
+let hasCriminalBlacklist = false;
+
+let isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
+
+
+
+
